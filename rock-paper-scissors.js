af996ced43eddff3b735e5e737eaf3df3bacd332
h-maren <26232423+h-maren@users.noreply.github.com>
2024-06-05 20:34:00 -0400

    Add getHumanChoice function Adds getHumanChoice function to ask for user input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,7 +4,7 @@ console.log("hello, test");
 //use random nnumber generator to pick value between 0,1,2, which each correspond to a string value
 function getComputerChoice(){
     let computerNumber=Math.floor(Math.random()*3);
-    console.log(computerNumber);
+    //console.log(computerNumber);
     let computerChoice="";
     switch(computerNumber){
         case 0:
@@ -18,4 +18,20 @@ function getComputerChoice(){
             break;
     }
     console.log(computerChoice);
+}
+
+//use prompt to get human choice for rock paper scissors making sure user picks valid choice
+function getHumanChoice(){
+    let check=1;
+    while(check=1){
+        let humanChoice=prompt("Please pick rock paper or scissors!").toLowerCase();
+        //console.log(humanChoice);
+        if ((humanChoice=="rock")|(humanChoice=="paper")|(humanChoice=="scissors")){
+            check=0;
+            return humanChoice;
+        } else {
+        alert("You didn't pick a valid value! Please enter rock paper or scissors!");
+        }
+    }
+    //console.log(humanChoice);
 }
