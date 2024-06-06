fc72c249460b6fd5fba2d13a9c1503c62302a7a2
h-maren <26232423+h-maren@users.noreply.github.com>
2024-06-05 21:36:58 -0400

    Add playRound function Made playRound function to play between human and machine

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,3 @@
-console.log("hello, test");
-
 //function called getComputerChoice to randomly return string rock paper or scissors
 //use random nnumber generator to pick value between 0,1,2, which each correspond to a string value
 function getComputerChoice(){
@@ -35,3 +33,31 @@ function getHumanChoice(){
     }
     //console.log(humanChoice);
 }
+
+//initialize variables humanScore and computerScore in game;
+let humanScore=0;
+let computerScore=0;
+
+//function to play single round which takes human choice and computerchoice and increments score based on what values are
+
+//cases (comp vs human): rock v rock, rock v paper, rock v scissors, paper v rock, paper v paper, paper v scissors, scissors v rock, scissors v paper, scissors v scissors
+//cases where comp wins: rock v scissors, paper v rock, scissors v paper
+//cases where human wins: rock v paper, paper v scissors, scissors v rock
+//cases where there's a tie: rock v rock, paper v paper, scissors v scissors
+
+function playRound(humanChoice,computerChoice){
+    if(((computerChoice=="rock")&&(humanChoice=="scissors"))|((computerChoice=="paper")&&(humanChoice=="rock"))|((computerChoice=="scissors")&&(humanChoice=="paper"))){
+        alert("computer wins this round!");
+        computerScore++;
+    }
+    else if(((computerChoice=="rock")&&(humanChoice=="paper"))|((computerChoice=="paper")&&(humanChoice=="scissors"))|((computerChoice=="scissors")&&(humanChoice=="rock"))){
+        alert("human wins this round!");
+        humanScore++;
+    }
+    else {
+        alert("a tie!");
+    }
+    console.log(computerScore);
+    console.log(humanScore);
+}
+
